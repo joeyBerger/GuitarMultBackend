0b15b25b9c6ec90a9c3523faeb31757dcc9e8747
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2020-10-30 20:58:30 -0700

    333

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
+    .use(express.static(path.join(__dirname, 'public')))
 //   .set('views', path.join(__dirname, 'views'))
 //   .set('view engine', 'ejs')
 //   .get('/', (req, res) => res.render('pages/index'))
-.get('/', (req, res) => {
-    res.send(JSON.stringify({'Hello World!' : "test"}))
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .get('/', (req, res) => {
+        res.send(JSON.stringify({'Hello World!' : "test"}))
+    })
+    .post('/test', (req,res) => {
+        console.log(req.body)
+        res.send(req.body)
+        // res.send(JSON.stringify({'test':'test'}))
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
